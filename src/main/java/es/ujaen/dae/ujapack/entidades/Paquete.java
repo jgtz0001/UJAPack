8781fb0841e6cab6fd85e2e952be0bf7ad8c15a6
paloro0 <pablolozano50@gmail.com>
2021-02-04 17:07:33 +0100

    probando

--- src/main/java/es/ujaen/dae/ujapack/entidades/Paquete.java
+++ src/main/java/es/ujaen/dae/ujapack/entidades/Paquete.java
@@ -4,7 +4,6 @@
 * and open the template in the editor.
  */
 package es.ujaen.dae.ujapack.entidades;
-
 import es.ujaen.dae.ujapack.excepciones.PuntoDeControlEquivocado;
 import java.io.Serializable;
 import java.time.LocalDateTime;
